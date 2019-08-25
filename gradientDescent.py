47178c764f6cc9cdf22dc34b4203dab31bd62040
jamalmohamad <mohammed.jamalhadi@gmail.com>
2019-08-25 20:38:21 +0200

    Let's start with 1000 iterations and a learning rate of 0.01 with theta from gaussian distribution

--- gradientDescent.py
+++ gradientDescent.py
@@ -40,5 +40,15 @@ def gradient_descent(X, y, theta, learning_rate, iterations):
 		cost_history[it] = cal_cost(theta, X, y)
 	return theta, cost_history, theta_history
 
+# Let's start with 1000 iterations and a learning rate of 0.01
+learningRate  = 0.1
+no_iterations = 1000
+# start with theta from gaussian distribution
+theta = np.random.randn(2, 1)
 
+X_b = np.c_[np.ones((len(X), 1)), X]
+theta, cost_history, theta_history = gradient_descent(X_b, y, theta, learningRate, no_iterations)
+
+print('Theta0 (predicted) (b):          {:0.3f},\nTheta1 (predicted) (m):          {:0.3f}'.format(theta[0][0],theta[1][0]))
+print('Final cost/MSE:  {:0.3f}'.format(cost_history[-1]))
 #plt.show()
